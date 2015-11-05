3e0a2eea0d30d3656a3f937d89b6af7c7c55744e
0xADADA <0xadada.pub@0xadada.pub>
2015-11-05 12:50:23 -0500

    Logs now go to  and  for gunicorn and nginx

--- docker/django/prod/gunicorn.conf.py
+++ docker/django/prod/gunicorn.conf.py
@@ -2,6 +2,8 @@ import multiprocessing
 
 bind = "0.0.0.0:8080"
 workers = multiprocessing.cpu_count() * 2 + 1
-accesslog = '/var/log/gunicorn/access.log'
 loglevel = 'info'
-errorlog = '/var/log/gunicorn/error.log'
+# Log to stdout
+accesslog = '-'
+# Log to stderr
+errorlog = '-'
